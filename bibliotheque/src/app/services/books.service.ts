69171de51d584d16c8a16573fe34aaa69a60cbaa
Shunyan <shunyan36@hotmail.fr>
2019-09-19 10:59:32 +0200

    Ajouter/modifier/supprimer book

--- bibliotheque/src/app/services/books.service.ts
+++ bibliotheque/src/app/services/books.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Book } from '../models/book.model';
+import * as firebase from 'firebase';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class BooksService {
 
-  constructor() { }
+  constructor() {
+      this.getBooks();
+  }
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data: DataSnapshot) => {
+          this.books = data.val() ? data.val() : [];
+          this.emitBooks();
+        }
+      );
+  }
+
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data: DataSnapshot) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  createNewBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  removeBook(book: Book) {
+    const bookIndexToRemove = this.books.findIndex(
+      (bookEl) => {
+        if(bookEl === book) {
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
 }
